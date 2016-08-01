9f2f5e4d54cec7fcffc8e916899871d29051914d
Ashish Patel <ashishpatel0720@gmail.com>
2016-07-31 21:02:16 -0700

    my soft files

---
+++ Competetive Codes/coding_template.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define llu long long unsigned int
+#define ll long long int
+#define mp make_pair
+#define pr(n)  printf("%d",n)
+#define prl(n) printf("%lld",n)
+#define prf(n) printf("%f",n)
+#define nl printf("\n")
+#define sp printf(" ")
+#define sc(n)  scanf("%d",&n)
+#define scl(n) scanf("%lld",&n)
+#define scf(n) scanf("%f",&n)
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define vi vector<int>
+#define vl vector<long>
+using namespace std;
+template <typename T>
+void show_2d(T container){
+    for(auto it=container.begin();it!=container.end();it++){
+        for(auto it2=it->begin();it2!=it->end();it2++)
+            cout<<*it2<<" ";
+        cout<<endl;
+    }
+    cout<<endl;
+}
+template <typename T>
+void show_pair(T container){
+    for(auto it=container.begin();it!=container.end();it++){
+        cout<<it->first<<" --> "<<it->second;
+        cout<<endl;
+    }
+    cout<<endl;
+}
+template <typename T>
+void show_1d(T container){
+
+    for(auto it=container.begin();it!=container.end();it++){
+        cout<<*it<<" ";
+
+    }
+    cout<<endl;
+}
+using namespace std;
+void show_arr(int arr[],int size){for (int i = 0; i < size; ++i)cout<<arr[i]<<" ";cout<<endl;}
+void solve(int n){
+
+}
+int main()
+{
+    int t;
+    sc(t);
+    while(t--){
+
+
+        nl;
+    }
+    return 0;
+}
